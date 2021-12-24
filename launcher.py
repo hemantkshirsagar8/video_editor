a8ebcc3c48e162c616dbb006a0bc8b2b8f7a8477
hemantkshirsagar8 <hemantkshirsagar8@gmail.com>
2021-12-24 21:36:34 +0530

    Completed logic for Video parts

--- launcher.py
+++ launcher.py
@@ -3,6 +3,7 @@ import streamlit as st
 # import pandas as pd
 from datetime import date
 from video_editor import VideoEditor
+from moviepy.editor import *
 
 st.title("Personal video editor")
 st.write("Author: Hemant Kshirsagar")
@@ -17,26 +18,28 @@ def video_parts(video, parts):
     video_editor.video_cutter()
 
 
-if st.sidebar.button("Video parts."):
-    st.write("Here, you can cut the video into multiple parts in sequence.")
+# if st.sidebar.button("Video parts."):
+st.write("Here, you can cut the video into multiple parts in sequence.")
 
-    with st.form('Video Parts'):
-        st.markdown('### Video Parts')
-        uploaded_file = st.file_uploader("Upload video a file.")
-        parts = st.number_input('Number of parts.', min_value=1, max_value=10, step=1)
-        if uploaded_file is not None:
-            st.write("28")
-            bytes_data = uploaded_file.getvalue()
-            st.write(bytes_data)
-            filename = uploaded_file.name
-            st.write(filename)
+with st.form('Video Parts'):
+    st.markdown('### Video Parts')
+    uploaded_file = st.file_uploader("Upload video a file.")
 
-        cutter_btn = st.form_submit_button('Start making parts')
-        if cutter_btn:
-            if parts is not None:
-                video_parts(uploaded_file, parts)
-            else:
-                st.warning("Please enter the number of parts.")
+    parts = st.number_input('Number of parts.', min_value=1, max_value=10, step=1)
+    st.write(parts)
+    if uploaded_file is not None:
+        bytes_data = uploaded_file.getvalue()
+        if not os.path.isdir(os.path.join(os.getcwd() + "/upload/")):
+            os.mkdir(os.path.join(os.getcwd() + "/upload/"))
+        with open("upload/video_file.mp4", "wb") as video_file:
+            video_file.write(bytes_data)
+
+    cutter_btn = st.form_submit_button('Start making parts')
+    if cutter_btn:
+        if parts is not None:
+            video_parts("upload/video_file.mp4", parts)
         else:
-            st.warning("File not uploaded.")
+            st.warning("Please enter the number of parts.")
+    else:
+        st.warning("File not uploaded.")
 
--- video_editor.py
+++ video_editor.py
@@ -1,6 +1,11 @@
+import os
 import streamlit as st
 from moviepy.editor import *
 from time import sleep
+from zipfile import ZipFile
+from io import BytesIO
+import base64
+import shutil
 
 
 class VideoEditor:
@@ -11,6 +16,8 @@ class VideoEditor:
 
     def video_cutter(self):
         st.info("Video cutter initiated...")
+        if not os.path.isdir(os.path.join(os.getcwd() + "/download/")):
+            os.mkdir(os.path.join(os.getcwd() + "/download/"))
         current_duration = VideoFileClip(self.video).duration
         single_duration = current_duration/self.divide_into_part
         current_video = f"{current_duration}.mp4"
@@ -20,8 +27,29 @@ class VideoEditor:
             current_duration -= single_duration
             current_video = f"{current_duration}.mp4"
     #         clip = clip.resize( (600,750) ) # instagram 4:5 aspect ratio
-            clip.to_videofile("new_" + current_video, codec="libx264", temp_audiofile='temp-audio.m4a', remove_temp=True, audio_codec='aac')
+            clip.to_videofile("download/new_" + current_video, codec="libx264", temp_audiofile='upload-audio.m4a', remove_temp=True, audio_codec='aac')
+        os.remove("upload/video_file.mp4")
         st.success("Video parts made successfully.")
+
+        zipObj = ZipFile("sample.zip", "w")
+        # Add multiple files to the zip
+
+        filenames = [entry.name for entry in sorted(os.scandir("download/"),
+                                                    key=lambda x: x.stat().st_mtime, reverse=True)]
+        for file in filenames:
+            zipObj.write("download/"+file)
+
+        zipObj.close()
+        ZipfileDotZip = "sample.zip"
+
+        with open(ZipfileDotZip, "rb") as f:
+            bytes = f.read()
+            b64 = base64.b64encode(bytes).decode()
+            href = f"<a href=\"data:file/zip;base64,{b64}\" download='{ZipfileDotZip}.zip'>\
+                Download all parts\
+            </a>"
+        shutil.rmtree("download/")
+        st.markdown(href, unsafe_allow_html=True)
 
     def add_audio_to_video(self):
         video_clip = VideoFileClip(self.video)
