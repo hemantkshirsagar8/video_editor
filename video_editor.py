fb27b5093466c9cd384bc7a9a6d21538ae3479c8
hemantkshirsagar8 <hemantkshirsagar8@gmail.com>
2021-12-25 12:41:37 +0530

    Exception handling

--- video_editor.py
+++ video_editor.py
@@ -56,37 +56,47 @@ class VideoEditor:
         st.markdown(href, unsafe_allow_html=True)
 
     def video_cutter(self):
-        st.info("Initiated...")
-        current_duration = VideoFileClip(self.video).duration
-        single_duration = current_duration/self.divide_into_part
-        # current_video = f"{current_duration}.mp4"
+        try:
+            st.info("Initiated...")
+            current_duration = VideoFileClip(self.video).duration
+            single_duration = current_duration/self.divide_into_part
+            # current_video = f"{current_duration}.mp4"
 
-        while current_duration > single_duration:
-            clip = VideoFileClip(self.video).subclip(current_duration-single_duration, current_duration)
-            current_duration -= single_duration
-            current_video = f"{self.divide_into_part - 1}.mp4"
-    #         clip = clip.resize( (600,750) ) # instagram 4:5 aspect ratio
-            clip.to_videofile(os.path.join(self.base_path, self.unique_id, "download") + "/part_" + current_video,
-                              codec="libx264", temp_audiofile='upload-audio.m4a',
-                              remove_temp=True, audio_codec='aac')
-            self.divide_into_part = self.divide_into_part - 1
-        st.success("Video parts made successfully.")
+            while current_duration > single_duration:
+                clip = VideoFileClip(self.video).subclip(current_duration-single_duration, current_duration)
+                current_duration -= single_duration
+                current_video = f"{self.divide_into_part - 1}.mp4"
+        #         clip = clip.resize( (600,750) ) # instagram 4:5 aspect ratio
+                clip.to_videofile(os.path.join(self.base_path, self.unique_id, "download") + "/part_" + current_video,
+                                  codec="libx264", temp_audiofile='upload-audio.m4a',
+                                  remove_temp=True, audio_codec='aac')
+                self.divide_into_part = self.divide_into_part - 1
+            st.success("Video parts made successfully.")
 
-        self.zip_and_download()
+            self.zip_and_download()
+        except Exception as e:
+            shutil.rmtree(os.path.join(self.base_path, self.unique_id))
+            print(str(e))
+            st.error("Something went wrong.")
 
     def add_audio_to_video(self):
-        st.info("Initiated...")
-        video_clip = VideoFileClip(self.video)
-        video_duration = video_clip.duration
-        background_audio_clip = AudioFileClip(self.audio)
-        audio_duration = background_audio_clip.duration
-        if audio_duration > video_duration:
-            bg_music = background_audio_clip.subclip(0, video_duration)
-        else:
-            bg_music = background_audio_clip.subclip(0, audio_duration)
-        final_clip = video_clip.set_audio(bg_music)
-    #     final_clip = final_clip.resize( (600,750) ) # instagram 4:5 aspect ratio
-        final_clip.write_videofile(os.path.join(self.base_path, self.unique_id, "download") + "/New_Audio_video.mp4",
-                                   codec='libx264', audio_codec="aac")
-        self.zip_and_download()
+        try:
+            st.info("Initiated...")
+            video_clip = VideoFileClip(self.video)
+            video_duration = video_clip.duration
+            background_audio_clip = AudioFileClip(self.audio)
+            audio_duration = background_audio_clip.duration
+            if audio_duration > video_duration:
+                bg_music = background_audio_clip.subclip(0, video_duration)
+            else:
+                bg_music = background_audio_clip.subclip(0, audio_duration)
+            final_clip = video_clip.set_audio(bg_music)
+            # final_clip = final_clip.resize( (600,750) ) # instagram 4:5 aspect ratio
+            final_clip.write_videofile(os.path.join(self.base_path, self.unique_id, "download") + "/New_Audio_video.mp4",
+                                       codec='libx264', audio_codec="aac")
+            self.zip_and_download()
+        except Exception as e:
+            shutil.rmtree(os.path.join(self.base_path, self.unique_id))
+            print(str(e))
+            st.error("Something went wrong.")
 
