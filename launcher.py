ae7dab3aa1687595928a93067e4486efa5bbe48c
hemantkshirsagar8 <hemantkshirsagar8@gmail.com>
2021-12-25 12:30:30 +0530

    Added Audio logic

--- launcher.py
+++ launcher.py
@@ -4,6 +4,7 @@ import streamlit as st
 from datetime import date
 from video_editor import VideoEditor
 from moviepy.editor import *
+import uuid
 
 st.title("Personal video editor")
 st.write("Author: Hemant Kshirsagar")
@@ -11,32 +12,74 @@ st.write("Author: Hemant Kshirsagar")
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
 
-def video_parts(video, parts):
+def video_parts(video, parts, unique_id, base_path):
     video_editor = VideoEditor()
     video_editor.video = video
     video_editor.divide_into_part = parts
+    video_editor.unique_id = unique_id
+    video_editor.base_path = base_path
     video_editor.video_cutter()
 
 
-# if st.sidebar.button("Video parts."):
-st.write("Here, you can cut the video into multiple parts in sequence.")
+def add_audio_in_video(video, audio, unique_id, base_path):
+    video_editor = VideoEditor()
+    video_editor.video = video
+    video_editor.audio = audio
+    video_editor.unique_id = unique_id
+    video_editor.base_path = base_path
+    video_editor.add_audio_to_video()
 
-with st.form('Video Parts'):
-    st.markdown('### Video Parts')
-    uploaded_file = st.file_uploader("Upload video a file.")
-    parts = st.number_input('Number of parts.', min_value=1, max_value=10, step=1)
-    parts = parts + 1
-    if uploaded_file is not None:
-        bytes_data = uploaded_file.getvalue()
-        if not os.path.isdir(os.path.join(os.getcwd() + "/upload/")):
-            os.mkdir(os.path.join(os.getcwd() + "/upload/"))
-        with open("upload/video_file.mp4", "wb") as video_file:
-            video_file.write(bytes_data)
 
-    cutter_btn = st.form_submit_button('Start making parts')
-    if cutter_btn:
-        if parts is not None:
-            video_parts("upload/video_file.mp4", parts)
-        else:
-            st.warning("Please enter the number of parts.")
+col1, col2 = st.columns(2)
 
+with col1:
+    with st.form('Video Parts'):
+        st.markdown('### Video Parts')
+        st.write("Here, you can cut the video into multiple parts in sequence.")
+        uploaded_file = st.file_uploader("Upload video a file.", type="mp4")
+        parts = st.number_input('Number of parts.', min_value=1, max_value=10, step=1)
+        parts = parts + 1
+        cutter_btn = st.form_submit_button('Start making parts')
+        if cutter_btn and parts is not None:
+            if uploaded_file is not None:
+                bytes_data = uploaded_file.getvalue()
+                unique_id = uuid.uuid4().hex
+                if not os.path.isdir(os.path.join(os.getcwd(), "temp")):
+                    os.mkdir(os.path.join(os.getcwd(), "temp"))
+                os.mkdir(os.path.join(os.getcwd(), "temp", unique_id))
+                os.mkdir(os.path.join(os.getcwd(), "temp", unique_id, "upload"))
+                os.mkdir(os.path.join(os.getcwd(), "temp", unique_id, "download"))
+                base_path = os.path.join(os.getcwd(), "temp")
+                with open(os.path.join(os.getcwd(), "temp", unique_id, "upload") + "/input_video.mp4", "wb") as video_file:
+                    video_file.write(bytes_data)
+                input_video_file_path = os.path.join(os.getcwd(), "temp", unique_id, "upload") + "/input_video.mp4"
+
+            video_parts(input_video_file_path, parts, unique_id, base_path)
+
+with col2:
+    with st.form('Add Audio in Video'):
+        st.markdown('### Add Audio in Video')
+        st.write("Here, you can add any mp3 audio into video file.")
+        uploaded_video_file = st.file_uploader("Upload video a file.", type="mp4")
+        uploaded_audio_file = st.file_uploader("Upload audio a file.", type="mp3")
+
+        start_btn = st.form_submit_button('Start process')
+        if start_btn:
+            if uploaded_video_file is not None and uploaded_audio_file is not None:
+                video_bytes_data = uploaded_video_file.getvalue()
+                audio_bytes_data = uploaded_audio_file.getvalue()
+                unique_id = uuid.uuid4().hex
+                if not os.path.isdir(os.path.join(os.getcwd(), "temp")):
+                    os.mkdir(os.path.join(os.getcwd(), "temp"))
+                os.mkdir(os.path.join(os.getcwd(), "temp", unique_id))
+                os.mkdir(os.path.join(os.getcwd(), "temp", unique_id, "upload"))
+                os.mkdir(os.path.join(os.getcwd(), "temp", unique_id, "download"))
+                base_path = os.path.join(os.getcwd(), "temp")
+                with open(os.path.join(os.getcwd(), "temp", unique_id, "upload") + "/input_video.mp4", "wb") as video_file:
+                    video_file.write(video_bytes_data)
+                with open(os.path.join(os.getcwd(), "temp", unique_id, "upload") + "/input_audio.mp3", "wb") as audio_file:
+                    audio_file.write(audio_bytes_data)
+                video_file_path = os.path.join(os.getcwd(), "temp", unique_id, "upload") + "/input_video.mp4"
+                audio_file_path = os.path.join(os.getcwd(), "temp", unique_id, "upload") + "/input_audio.mp3"
+
+            add_audio_in_video(video_file_path, audio_file_path, unique_id, base_path)
--- video_editor.py
+++ video_editor.py
@@ -1,4 +1,5 @@
 import os
+from os.path import basename
 import streamlit as st
 from moviepy.editor import *
 from time import sleep
@@ -13,45 +14,68 @@ class VideoEditor:
         self.video = None
         self.audio = None
         self.divide_into_part = None
+        self.unique_id = None
+        self.base_path = None
+
+    def del_unwwanted_files(self, f_type):
+        files_in_dir = os.listdir("./")
+        filtered_files = [file for file in files_in_dir if file.endswith(f_type)]
+        for file in filtered_files:
+            os.remove("./" + file)
+
+    # Zip the files from given directory that matches the filter
+    def zipFilesInDir(self, dirName, zipFileName, filter):
+        # create a ZipFile object
+        with ZipFile(zipFileName, 'w') as zipObj:
+            # Iterate over all the files in directory
+            for folderName, subfolders, filenames in os.walk(dirName):
+                for filename in filenames:
+                    if filter(filename):
+                        # create complete filepath of file in directory
+                        filePath = os.path.join(folderName, filename)
+                        # Add file to zip
+                        zipObj.write(filePath, basename(filePath))
+
+    def zip_and_download(self):
+        filenames = [entry.name for entry in
+                     sorted(os.scandir(os.path.join(self.base_path, self.unique_id, "download")),
+                            key=lambda x: x.stat().st_mtime, reverse=True)]
+        self.zipFilesInDir(
+            "temp/" + self.unique_id + "/download/", "temp/" + self.unique_id + ".zip", lambda name: ".mp4" in name)
+
+        ZipfileDotZip = self.unique_id + ".zip"
+
+        with open(self.base_path + "/" + ZipfileDotZip, "rb") as f:
+            bytes = f.read()
+            b64 = base64.b64encode(bytes).decode()
+            href = f"<a href=\"data:file/zip;base64,{b64}\" download='{ZipfileDotZip}.zip'>\
+                        Download all parts\
+                    </a>"
+        shutil.rmtree(os.path.join(self.base_path, self.unique_id))
+        self.del_unwwanted_files(".mp4")
+        st.markdown(href, unsafe_allow_html=True)
 
     def video_cutter(self):
-        st.info("Video cutter initiated...")
-        if not os.path.isdir(os.path.join(os.getcwd() + "/download/")):
-            os.mkdir(os.path.join(os.getcwd() + "/download/"))
+        st.info("Initiated...")
         current_duration = VideoFileClip(self.video).duration
         single_duration = current_duration/self.divide_into_part
-        current_video = f"{current_duration}.mp4"
+        # current_video = f"{current_duration}.mp4"
 
         while current_duration > single_duration:
             clip = VideoFileClip(self.video).subclip(current_duration-single_duration, current_duration)
             current_duration -= single_duration
-            current_video = f"{current_duration}.mp4"
+            current_video = f"{self.divide_into_part - 1}.mp4"
     #         clip = clip.resize( (600,750) ) # instagram 4:5 aspect ratio
-            clip.to_videofile("download/new_" + current_video, codec="libx264", temp_audiofile='upload-audio.m4a', remove_temp=True, audio_codec='aac')
-        os.remove("upload/video_file.mp4")
+            clip.to_videofile(os.path.join(self.base_path, self.unique_id, "download") + "/part_" + current_video,
+                              codec="libx264", temp_audiofile='upload-audio.m4a',
+                              remove_temp=True, audio_codec='aac')
+            self.divide_into_part = self.divide_into_part - 1
         st.success("Video parts made successfully.")
 
-        zipObj = ZipFile("sample.zip", "w")
-        # Add multiple files to the zip
-
-        filenames = [entry.name for entry in sorted(os.scandir("download/"),
-                                                    key=lambda x: x.stat().st_mtime, reverse=True)]
-        for file in filenames:
-            zipObj.write("download/"+file)
-
-        zipObj.close()
-        ZipfileDotZip = "sample.zip"
-
-        with open(ZipfileDotZip, "rb") as f:
-            bytes = f.read()
-            b64 = base64.b64encode(bytes).decode()
-            href = f"<a href=\"data:file/zip;base64,{b64}\" download='{ZipfileDotZip}.zip'>\
-                Download all parts\
-            </a>"
-        shutil.rmtree("download/")
-        st.markdown(href, unsafe_allow_html=True)
+        self.zip_and_download()
 
     def add_audio_to_video(self):
+        st.info("Initiated...")
         video_clip = VideoFileClip(self.video)
         video_duration = video_clip.duration
         background_audio_clip = AudioFileClip(self.audio)
@@ -62,6 +86,7 @@ class VideoEditor:
             bg_music = background_audio_clip.subclip(0, audio_duration)
         final_clip = video_clip.set_audio(bg_music)
     #     final_clip = final_clip.resize( (600,750) ) # instagram 4:5 aspect ratio
-        final_clip.write_videofile("new_" + self.video, codec='libx264', audio_codec="aac")
-    #     final_clip.ipython_display(width=280)
+        final_clip.write_videofile(os.path.join(self.base_path, self.unique_id, "download") + "/New_Audio_video.mp4",
+                                   codec='libx264', audio_codec="aac")
+        self.zip_and_download()
 
